23f12bb67afd5cd663fb432451348bb4c832389c
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-20 16:29:11 -0600

    Added middleware to ensure users are authenticated.

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 
 router.post(
   '',
+  checkAuth,
   multer({ storage: storage }).single('image'),
   (req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
@@ -90,6 +92,7 @@ router.get('/:id', (req, res, next) => {
 
 router.put(
   '/:id',
+  checkAuth,
   multer({ storage: storage }).single('image'),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -110,7 +113,7 @@ router.put(
   }
 );
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(
     res.status(204).json({ message: 'Post deleted.' })
   );
