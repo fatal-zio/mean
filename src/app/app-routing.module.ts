e36a1aee071e08d0e5e2d56086a5ebaf6fb49e0c
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-20 11:34:11 -0600

    Added basic skeleton forms for Login and Signup.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './features/posts/post-list/post-list.component';
 import { PostCreateComponent } from './features/posts/post-create/post-create.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent },
   { path: 'edit/:id', component: PostCreateComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
