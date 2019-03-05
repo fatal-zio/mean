27abc1c746b90175c8043df259718fe036baf7da
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-03-05 15:22:33 -0600

    Moved File logic to its own middleware.

--- api/routes/post-routes.js
+++ api/routes/post-routes.js
@@ -1,52 +1,17 @@
 const express = require('express');
-const multer = require('multer');
 const router = express.Router();
 
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 const PostController = require('../controllers/post-controller');
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mime type.');
-    if (isValid) {
-      error = null;
-    }
-    cb(error, 'api/images');
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(' ')
-      .join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-});
-
-router.post(
-  '',
-  checkAuth,
-  multer({ storage: storage }).single('image'),
-  PostController.createPost
-);
+router.post('', checkAuth, extractFile, PostController.createPost);
 
 router.get('', PostController.getPosts);
 
 router.get('/:id', PostController.getPost);
 
-router.put(
-  '/:id',
-  checkAuth,
-  multer({ storage: storage }).single('image'),
-  PostController.updatePost
-);
+router.put('/:id', checkAuth, extractFile, PostController.updatePost);
 
 router.delete('/:id', checkAuth, PostController.deletePost);
 
